2b6139c739e083c1206cbd3e07e6e4167be278ee
Rey-TryToBetter <mohammad.rayhan3645@smk.belajar.id>
2025-05-31 15:35:45 +0700

    tgl 31

---
+++ modul-4-type-conversion/index.js
@@ -0,0 +1,18 @@
+//implicit conversion
+// let result = "5" + 10
+let result = "5" - 10
+
+//falsy value 0, null, undefined, ""
+let bool = !0
+
+//Eksplicit conversion
+let num = 100
+let string = String(num)
+
+let string2 = "200.2000"
+// let num2 = parseInt(string2)
+let num2 = parseFloat(string2)
+
+let bool2 = Boolean(0)
+
+console.log(bool2, typeof bool2)
