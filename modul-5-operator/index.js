7ae09bac3a7f2e6f9f9461fae1a622dc96835e26
Rey-TryToBetter <mohammad.rayhan3645@smk.belajar.id>
2025-06-02 14:13:04 +0700

    02 - 06 - 2025

---
+++ modul-5-operator/index.js
@@ -0,0 +1,43 @@
+//operator aritmatika
+
+//penjumlahan
+
+let x = 10
+let y = 5
+let sum = x + y //outputnya = 15
+
+
+//pengurangan
+let a = 20
+let b = 5
+let suman = a - b //outputna 15
+
+//operasi perkalian
+let c = 5
+let d = 5
+let anjay = c * d
+
+
+//pembagian
+let t = 20
+let l = 2
+let pembagian = t / l
+
+//modulus
+let g = 15
+let v = 4
+let modulus = g % v
+
+//eksponen
+let s = 2
+let p = 3
+let eksponen = s ** p
+
+//increment
+let count = 10
+count++
+
+//decrement
+let countDown = 5
+countDown--
+console.log(countDown , typeof countDown)
---
+++ modul-5-operator/scriptAsignment.js
@@ -0,0 +1,37 @@
+//Assignment operator
+
+//Assignment Dasar
+let nama = "rey"
+console.log(nama, typeof nama)
+
+//Assignment  dan penjumlahan
+let x = 7
+// x = x + 9 //sebenarnya sama aja tetapi, akan lebih mudah jika mengguanakan +=
+x += 100
+console.log(x, typeof x)
+
+//Assignment dan pengurangan
+let a = 10
+// a = a - 9  //sebenarnya sama aja tetapi, akan lebih mudah jika mengguanakan -=
+a -= 3
+console.log(a, typeof a)
+
+//assignment dan perkalian
+let b = 5
+// a = a * 9  //sebenarnya sama aja tetapi, akan lebih mudah jika mengguanakan *=
+b *= 5
+console.log(b, typeof b)
+
+//assignment dan pembagian
+let c = 10
+// a = a / 9  //sebenarnya sama aja tetapi, akan lebih mudah jika mengguanakan /=
+c /= 2
+console.log(c, typeof c)
+
+//assignment dan  modulus
+let d = 26
+// a = a % 9  //sebenarnya sama aja tetapi, akan lebih mudah jika mengguanakan %=
+d %= 5
+console.log(d, typeof d)
+
+//asigment dan 
