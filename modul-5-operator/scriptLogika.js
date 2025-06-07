3bccec5d336ba25cff32a382d1da74892f29f041
Rey-TryToBetter <mohammad.rayhan3645@smk.belajar.id>
2025-06-08 00:12:48 +0700

    08-06-26

---
+++ modul-5-operator/scriptLogika.js
@@ -0,0 +1,17 @@
+//operator logika
+
+//And && operator
+let a = true
+let b = true // jika salah satunya false maka outputnya akan false
+let result = a && b
+
+//OR operator ||
+let ab = true
+let ac = true
+let orResult = ab || ac
+
+//NOT operator !
+let e = true
+let notResult = !e
+
+console.log(notResult)
---
+++ modul-5-operator/scriptOperator.js
@@ -0,0 +1,41 @@
+//comparison operatpr
+
+// == mengecek atau membandingkan persamaan nilai
+let a = "5"
+let b = 6
+console.log(a == b) //maka ouputna akan true, karena angkanya sama, walaupung tipe datanya berbeda
+
+// === mengecek atau membandingkan persamaan nilai dan tipe data
+let ab = "5"
+let ba = 5
+console.log(ab === ba) //maka hasilnya akan false, karena tipe datanya berbeda
+
+// != mengecek ketidaksamaan nilai dari variable
+let c = "5"
+let d = 5
+console.log(c != d)
+
+// !== mengecek atau membandingkan ketidaksamaan nilai dan tipe data variable
+let g = "5"
+let h = 5
+console.log(g !== h)
+
+// > mengecek apakah variable 1 lebih besar dari variable lainnya
+let o = 7
+let t = 2
+console.log(o > t) //outputnya menjadi true karena variable o lebih besar dari variable t
+
+// => mengecek apakah variable 1 lebih besar atau sama dari variable lainnya
+let y = 5
+let r = 4
+console.log(y >= r)
+
+// < mengecek apakah variable 1 ini lebih kecil dari variable lainnya
+let u = 2
+let w = 4
+console.log(u <= w)
+
+// <= mengecek apakah variable 1 ini lebih kecil atau sana dari variable lainnya
+let s = 5
+let v = 5
+console.log(s <= v)
